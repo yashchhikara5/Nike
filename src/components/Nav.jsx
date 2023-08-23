47aafe891aef86f9e8365c6db18a9d116604f270
yashchhikara5 <yashchhikara5@gmail.com>
2023-08-23 14:45:17 +0530

    created nav and landing page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import { headerLogo } from '../assets/images'
 import { hamburger } from '../assets/icons'
+import { navLinks } from '../constants'
 
 const Nav = () => {
   return (
@@ -9,8 +10,20 @@ const Nav = () => {
                 <img src={headerLogo} alt="Logo" width={130} height={29} />
             </a>
             <ul className='flex-1 flex justify-center items-center gap-16 mg-lg:hidden'>
-
+                {navLinks.map((item) => (
+                    <li key={item.label}>
+                        <a 
+                            href={item.href}
+                            className="font-montserrat leading-normal text-lg text-slate-gray"    
+                        >
+                            {item.label}
+                        </a>
+                    </li>
+                ))}
             </ul>
+            <div className='hidden max-lg:block'>
+                <img src={hamburger} alt="Hamburger" width={25} height={25} />
+            </div>
         </nav>
     </header>
   )
