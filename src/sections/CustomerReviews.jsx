997bf492e1cbdfda4ead97b7349091558644ceef
yashchhikara5 <yashchhikara5@gmail.com>
2023-08-24 07:01:43 +0530

    Added reviews

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,6 +1,28 @@
+import { reviews } from "../constants"
+import ReviewCard from "../components/ReviewCard"
+
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
+    <section className="max-container ">
+      <h3 className="font-palanquin font-bold text-center text-4xl">
+        What Our
+        <span className="text-coral-red"> Customers </span>
+        Say?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">Hear genuine stories from our satified customers about their exceptional experiences with us.</p>
+
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review) => (
+          <ReviewCard 
+            key={review.customerName}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
+        ))}
+      </div>
+    </section>
   )
 }
 
