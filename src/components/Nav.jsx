f8abe3139506db69ab719358485c7688aedb06b6
yashchhikara5 <yashchhikara5@gmail.com>
2023-08-24 08:16:27 +0530

    Added Footer

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,12 +4,17 @@ import { navLinks } from '../constants'
 
 const Nav = () => {
   return (
-    <header className='padding-x py-8'>
+    <header className='padding-x py-8 absolute z-10 w-full'>
         <nav className='flex justify-between items-center max-container'>
             <a href="/">
-                <img src={headerLogo} alt="Logo" width={130} height={29} />
+                <img 
+                    src={headerLogo} 
+                    alt="Logo" 
+                    width={130} 
+                    height={29} 
+                />
             </a>
-            <ul className='flex-1 flex justify-center items-center gap-16 mg-lg:hidden'>
+            <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
                 {navLinks.map((item) => (
                     <li key={item.label}>
                         <a 
@@ -21,6 +26,11 @@ const Nav = () => {
                     </li>
                 ))}
             </ul>
+            <div className='flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24'>
+          <a href='/'>Sign in</a>
+          <span>/</span>
+          <a href='/'>Explore now</a>
+        </div>
             <div className='hidden max-lg:block'>
                 <img src={hamburger} alt="Hamburger" width={25} height={25} />
             </div>
