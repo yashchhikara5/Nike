f090cacef7a09036530db4a0bfac83b8763199f3
yashchhikara5 <yashchhikara5@gmail.com>
2023-08-24 07:17:46 +0530

    Added the subscriptions

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,15 @@
-const Button = ({ label, iconURL, backgroundColor, textColor, borderColor}) => {
+const Button = ({ label, iconURL, backgroundColor, textColor, borderColor, fullWidth}) => {
   return (
     <button 
     className={`flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none rounded-full
     ${
       backgroundColor
     ? `${backgroundColor} ${textColor} ${borderColor}`
-    : "bg-coral-red  text-white border-coral-red"}`}>
+    : "bg-coral-red  text-white border-coral-red"}
+    ${
+      fullWidth && 'w-full' 
+    }
+    `}>
         {label}
 
         {iconURL && <img 
